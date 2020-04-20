0f17746cd39132e031e29ac7e699e15427fc6523
v-bui <v-bui@dimage.co.jp>
2020-04-20 15:34:25 +0700

    prototype scope

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -7,12 +7,18 @@ public class MainApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloApplication objectA = (HelloApplication) context.getBean("helloWorld");
+		HelloApplication objectA = (HelloApplication) context.getBean("examplePrototype");
 		
+		//lúc nào gọi thì đối một đối tượng mới được tạo ra.
 		objectA.setMessenger("This is example of SingleTon Bean: onbject A !");
 		objectA.getMessenger();
 		
-		HelloApplication objectB = (HelloApplication) context.getBean("helloWorld");
+		HelloApplication objectB = (HelloApplication) context.getBean("examplePrototype");
 		objectB.getMessenger();
+		/*
+		 * Output
+		 * Your messenger is : This is example of SingleTon Bean: onbject A !
+		 * Your messenger is : Hello World!
+		 */
 	}
 }
