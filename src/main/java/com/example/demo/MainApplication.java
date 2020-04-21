748f972db10cee1e4a8128cfd8e6089f50a423d8
v-bui <v-bui@dimage.co.jp>
2020-04-21 09:19:42 +0700

    inner-bean

--- src/main/java/com/example/demo/MainApplication.java
+++ src/main/java/com/example/demo/MainApplication.java
@@ -8,8 +8,8 @@ public class MainApplication {
 	public static void main(String[] args) {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
-		HelloApplication objectA = (HelloApplication) context.getBean("helloWorld");
-		objectA.getMessenger();
-		context.registerShutdownHook();
+//		HelloApplication objectA = (HelloApplication) context.getBean("spellChecker");
+//		objectA.getMessenger();
+//		context.registerShutdownHook();
 	}
 }
