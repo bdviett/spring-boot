f1ec2c08a58b2432c13319745e8392a5ad73d882
v-bui <v-bui@dimage.co.jp>
2020-04-22 16:48:51 +0700

    custom event

---
+++ src/main/java/custom/event/MainApp.java
@@ -0,0 +1,17 @@
+package custom.event;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ConfigurableApplicationContext context = 
+         new ClassPathXmlApplicationContext("custom-event.xml");
+	  
+      CustomEventPublisher cvp = 
+         (CustomEventPublisher) context.getBean("customEventPublisher");
+      
+      cvp.publish();  
+      cvp.publish();
+   }
+}
